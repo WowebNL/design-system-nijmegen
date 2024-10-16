e06c96a232446717206dd98645501b5815e8bbc1
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2024-10-16 13:49:48 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@6b43e59fde280ee0820687295cf13b8a45527c2c 🚀

---
+++ assets/Annotatie-C5WzWpUP.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as o}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";function r(t){const n=Object.assign({h1:"h1",h2:"h2",ul:"ul",li:"li",strong:"strong"},o(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{id:"annotatie-button-component",children:"Annotatie Button component"}),`
+`,e.jsx(n.h2,{id:"voor-redacteuren",children:"Voor redacteuren"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Primaire Knop (Primary Button):"}),`
+Zorg ervoor dat de tekstlabel duidelijk de hoofdactie beschrijft. Vermijd lange zinnen en houd het beknopt.`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Secundaire Knop (Secondary Button):"}),`
+Zorg voor heldere tekstlabels die de minder belangrijke acties accuraat weergeven. Beperk de tekst tot twee woorden indien mogelijk.`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Aanvullende Knop (Supplementary Button):"}),`
+Wees beknopt en expliciet bij het beschrijven van de minder zichtbare acties, zoals annuleren of negeren.
+Icongebruik: Zorg ervoor dat tekstlabels samen met pictogrammen worden gebruikt voor een beter begrip.
+Toegankelijkheid: Gebruik begrijpelijke taal en vermijd dubbelzinnigheden in tekstlabels.`]}),`
+`]})]})}function l(t={}){const{wrapper:n}=Object.assign({},o(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(r,t)})):r(t)}export{l as default};
---
+++ assets/Button.docs-B2i4786d.js
@@ -0,0 +1,24 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as i,P as m,b as r,e as l}from"./chunk-HLWAVYOI-lmzU_KTT.js";import{B as a}from"./Button.stories-y0vUXA8x.js";import{useMDXComponents as s}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C2ULgOjY.js";import"../sb-preview/runtime.js";import"./react-18-Bym-zv4b.js";import"./extends-CCbyfPlC.js";import"./index-VaGq4-f5.js";/* empty css              */function o(e){const n=Object.assign({h1:"h1",h2:"h2"},s(),e.components);return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:a}),`
+`,t.jsx(n.h1,{id:"button-component-implementation-in-htmlcss",children:"Button Component implementation in HTML/CSS"}),`
+`,t.jsx(n.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r,{}),`
+`,t.jsx(n.h2,{id:"example-implementation",children:"Example implementation"}),`
+`,t.jsx(l,{language:"html",code:`
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <title>Example page</title>
+    <link rel="stylesheet" href="node_modules/@utrecht/button-css/dist/index.css" />
+    <link rel="stylesheet" href="node_modules/@gemeentenijmegen/design-tokens/dist/index.css'" />
+    <link rel="stylesheet" href="node_modules/@gemeentenijmegen/font/dist/index.css'" />
+  </head>
+  <body>
+    <div class="nijmegen-theme">
+      <button class="utrecht-button" type="button">Button</button>
+    </div>
+  </body>
+</html>
+`})]})}function M(e={}){const{wrapper:n}=Object.assign({},s(),e.components);return n?t.jsx(n,Object.assign({},e,{children:t.jsx(o,e)})):o(e)}export{M as default};

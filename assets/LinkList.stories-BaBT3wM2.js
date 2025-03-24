02d510ecdca3dd913b6bfa9952aa1f0966215182
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-03-24 09:46:31 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@249421b28cc43cf880b77e79c862dc338464559f 🚀

---
+++ assets/LinkList.stories-BaBT3wM2.js
@@ -0,0 +1,17 @@
+import{j as s}from"./jsx-runtime-CKrituN3.js";import{L as l}from"./index-N71Teeq5.js";import{s as m,r as f,p as d}from"./server.browser-rAMDWieH.js";import{a as u}from"./_LinkList-C6rKkPeO.js";import{I as h}from"./IconChevronRight-DyYvLEvC.js";const k={title:"Components/Link list/Html Implementation",id:"html-link-list",component:l,argTypes:u,args:{},parameters:{status:{type:"BETA"},docs:{source:{transform:(p,n)=>{var t,o;const r=typeof n.component=="function"?n.component:typeof((t=n.component)==null?void 0:t.render)=="function"?(o=n.component)==null?void 0:o.render:null;return r?m.format(f(r(n.args)),{parser:"babel",plugins:[d]}):p}}}}},e={args:{icon:()=>s.jsx("span",{className:"utrecht-icon",children:s.jsx(h,{})}),links:[{children:"Link 1",href:"#"},{children:"Link 2",href:"#"},{children:"Link 3",href:"#"}]}};var a,i,c;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    icon: () => <span className="utrecht-icon">
+        <IconChevronRight />
+      </span>,
+    links: [{
+      children: 'Link 1',
+      href: '#'
+    }, {
+      children: 'Link 2',
+      href: '#'
+    }, {
+      children: 'Link 3',
+      href: '#'
+    }]
+  }
+}`,...(c=(i=e.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const L=["Default"],I=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:L,default:k},Symbol.toStringTag,{value:"Module"}));export{I as L};
---
+++ assets/LinkList.stories-Cj2ifHjA.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{L as i}from"./index-N71Teeq5.js";import{a}from"./_LinkList-C6rKkPeO.js";import{I as o}from"./IconChevronRight-DyYvLEvC.js";const c={title:"Components/Link list/React Implementation",id:"react-link-list",component:i,argTypes:a,args:{},parameters:{status:{type:"BETA"}}},n={name:"Link list",args:{icon:()=>e.jsx("span",{className:"utrecht-icon",children:e.jsx(o,{})}),links:[{children:"Link 1",href:"#"},{children:"Link 2",href:"#"},{children:"Link 3",href:"#"}]}};var r,t,s;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Link list',
+  args: {
+    icon: () => <span className="utrecht-icon">
+        <IconChevronRight />
+      </span>,
+    links: [{
+      children: 'Link 1',
+      href: '#'
+    }, {
+      children: 'Link 2',
+      href: '#'
+    }, {
+      children: 'Link 3',
+      href: '#'
+    }]
+  }
+}`,...(s=(t=n.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const l=["Default"],f=Object.freeze(Object.defineProperty({__proto__:null,Default:n,__namedExportsOrder:l,default:c},Symbol.toStringTag,{value:"Module"}));export{n as D,f as L};

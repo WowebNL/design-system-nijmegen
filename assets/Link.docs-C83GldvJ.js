38ec3b5d5c22e3a8712f03bad9adefae5532774a
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-03-26 21:11:47 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@22d903eac1f17682aad6eb89376a2f75b596dff3 🚀

--- assets/Link.docs-C83GldvJ.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as r,P as e,b as s}from"./chunk-HLWAVYOI-T1JXLePu.js";import{L as p}from"./Link.stories-80c5wY-L.js";import{useMDXComponents as m}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-xml9DUuJ.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-ToFVweKJ.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./index-N71Teeq5.js";import"./clsx-DxZ0A2-Z.js";import"./server.browser-rAMDWieH.js";import"./_Link-C6rKkPeO.js";import"./IconCalendarEvent-3BPlFUi5.js";import"./createReactComponent-D_5DJZVZ.js";import"./IconArrowRight-pMXm72se.js";function i(n){const o=Object.assign({h1:"h1",h2:"h2"},m(),n.components);return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(r,{of:p}),`
-`,t.jsx(o.h1,{id:"link-component-implementation-in-htmlcss",children:"Link Component implementation in HTML/CSS"}),`
-`,t.jsx(o.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
-`,t.jsx(e,{}),`
-`,t.jsx(s,{})]})}function _(n={}){const{wrapper:o}=Object.assign({},m(),n.components);return o?t.jsx(o,Object.assign({},n,{children:t.jsx(i,n)})):i(n)}export{_ as default};

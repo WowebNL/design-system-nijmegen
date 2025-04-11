88f789ac2eee5ade51a93942b1499847a5970a15
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-04-11 14:44:22 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@7fbe060d770e0b1a4a0e67e530dda77d8cfc565d 🚀

--- assets/Accordion.docs-CF9aM_BH.js
+++
@@ -1,8 +0,0 @@
-import{j as o}from"./jsx-runtime-CKrituN3.js";import{M as i,P as m,C as s}from"./chunk-HLWAVYOI-8TF0Pbqq.js";import{A as p}from"./Accordion.stories-B_HWZeKW.js";import{useMDXComponents as r}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BkaA6scL.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-ToFVweKJ.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./index-k1j14ZU1.js";import"./clsx-Du1eRyZV.js";import"./IconChevronDown-xLOYuVOv.js";import"./createReactComponent-D_5DJZVZ.js";function e(n){const t=Object.assign({h1:"h1",p:"p",a:"a",h2:"h2"},r(),n.components);return o.jsxs(o.Fragment,{children:[`
-`,`
-`,o.jsx(i,{of:p}),`
-`,o.jsx(t.h1,{id:"accordion-component-react-implementation",children:"Accordion Component React implementation"}),`
-`,o.jsxs(t.p,{children:["This documentation page shows the implementation of the Accordion component in ",o.jsx(t.a,{href:"https://react.dev/",target:"_blank",rel:"nofollow noopener noreferrer",children:"React"}),"."]}),`
-`,o.jsx(t.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
-`,o.jsx(m,{}),`
-`,o.jsx(s,{})]})}function X(n={}){const{wrapper:t}=Object.assign({},r(),n.components);return t?o.jsx(t,Object.assign({},n,{children:o.jsx(e,n)})):e(n)}export{X as default};

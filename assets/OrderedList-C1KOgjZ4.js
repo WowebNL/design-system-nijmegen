065a5c807340297c2781a38e4ac0c3372f5fb470
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-04-24 13:57:20 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@a49a2eeffccb3b9a09201b282a9f347367a6e5d0 🚀

---
+++ assets/OrderedList-C1KOgjZ4.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as i,P as m,C as s}from"./chunk-HLWAVYOI-C9yi7O0d.js";import{O as p}from"./OrderedList.stories-DSRZ-lf1.js";import{useMDXComponents as e}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Bzjz3v5D.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-ToFVweKJ.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./index-B96GDcmv.js";import"./clsx-Du1eRyZV.js";import"./server.browser-rAMDWieH.js";import"./_OrderedList-C6rKkPeO.js";function r(n){const o=Object.assign({h1:"h1",h2:"h2"},e(),n.components);return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:p}),`
+`,t.jsx(o.h1,{id:"ordered-list-component-implementation-in-htmlcss",children:"Ordered list Component implementation in HTML/CSS"}),`
+`,t.jsx(o.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(s,{})]})}function P(n={}){const{wrapper:o}=Object.assign({},e(),n.components);return o?t.jsx(o,Object.assign({},n,{children:t.jsx(r,n)})):r(n)}export{P as default};

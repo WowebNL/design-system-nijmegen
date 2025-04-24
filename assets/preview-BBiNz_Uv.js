d3049255b5f0f22e8bc51c3f76ef08c58bf13fe1
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-04-24 14:00:15 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@39c4c0d6038772aab9f3dbd6c6311ad562d67661 🚀

--- assets/preview-BBiNz_Uv.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-oilLJrzh.js","./chunk-HLWAVYOI-C9yi7O0d.js","./iframe-Bzjz3v5D.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-ToFVweKJ.js","./extends-CCbyfPlC.js","./_getPrototype-nBa_wnor.js","./index-ltOY3w3e.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as e}from"./iframe-Bzjz3v5D.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-oilLJrzh.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};

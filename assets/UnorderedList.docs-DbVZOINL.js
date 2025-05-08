c7c92d408749c252693ab9a2c8fdeb81531b09ef
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-05-08 18:28:52 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@bfeb6658baf5fd22f65c19db15f7b299b09ad32f 🚀

--- assets/UnorderedList.docs-DbVZOINL.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as i,P as m,C as s}from"./chunk-HLWAVYOI-Czzv44Nf.js";import{U as p}from"./UnorderedList.stories-ClS6jsLx.js";import{useMDXComponents as e}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DIrwDBfE.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-ToFVweKJ.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./index-B96GDcmv.js";import"./clsx-Du1eRyZV.js";import"./server.browser-rAMDWieH.js";import"./_UnorderedList-C6rKkPeO.js";function r(n){const o=Object.assign({h1:"h1",h2:"h2"},e(),n.components);return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:p}),`
-`,t.jsx(o.h1,{id:"unordered-list-component-implementation-in-htmlcss",children:"Unordered list Component implementation in HTML/CSS"}),`
-`,t.jsx(o.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
-`,t.jsx(m,{}),`
-`,t.jsx(s,{})]})}function y(n={}){const{wrapper:o}=Object.assign({},e(),n.components);return o?t.jsx(o,Object.assign({},n,{children:t.jsx(r,n)})):r(n)}export{y as default};

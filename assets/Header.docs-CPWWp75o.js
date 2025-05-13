aad9119d6e24e1564d228ef64cb16f03073ba6aa
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-05-13 10:58:38 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@61edafa7008ea6cf2f641958fc9a8ffdb0516493 🚀

---
+++ assets/Header.docs-CPWWp75o.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as m,P as i,C as s}from"./chunk-HLWAVYOI-xLhObPS_.js";import{H as p}from"./Header.stories-4D5LvGDl.js";import{useMDXComponents as e}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-M_2FmTzf.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-ToFVweKJ.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";/* empty css                  */function r(n){const o=Object.assign({h1:"h1"},e(),n.components);return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(m,{of:p}),`
+`,t.jsx(o.h1,{id:"header-component-implementation-in-htmlcss",children:"Header Component implementation in HTML/CSS"}),`
+`,t.jsx(i,{}),`
+`,t.jsx(s,{})]})}function X(n={}){const{wrapper:o}=Object.assign({},e(),n.components);return o?t.jsx(o,Object.assign({},n,{children:t.jsx(r,n)})):r(n)}export{X as default};
---
+++ assets/preview-BLr9AR_E.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-DCIwocIG.js","./chunk-HLWAVYOI-xLhObPS_.js","./iframe-M_2FmTzf.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-ToFVweKJ.js","./extends-CCbyfPlC.js","./_getPrototype-nBa_wnor.js","./index-ltOY3w3e.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as e}from"./iframe-M_2FmTzf.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-DCIwocIG.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};

21dc0521adee44ce7388555df9f6e466be6dd3e2
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-06-23 10:02:10 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@4cb7e79931cd51035a384d167943a3ab540a04ca 🚀

--- assets/Header.docs-CdoMulOR.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as i,P as e,C as s}from"./chunk-HLWAVYOI-WSDF--JV.js";import{H as p}from"./Header.stories-qheSn8Y5.js";import{useMDXComponents as m}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DP1ccv_d.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./Header-J3EfUcYG.js";/* empty css                  */import"./clsx-B-dksMZM.js";import"./server.browser-rAMDWieH.js";import"./index.min-B3hIxFls.js";import"./HtmlTransformer-CIQzRuZl.js";function r(o){const n=Object.assign({h1:"h1"},m(),o.components);return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:p}),`
-`,t.jsx(n.h1,{id:"header-component-implementation-in-htmlcss",children:"Header Component implementation in HTML/CSS"}),`
-`,t.jsx(e,{}),`
-`,t.jsx(s,{})]})}function S(o={}){const{wrapper:n}=Object.assign({},m(),o.components);return n?t.jsx(n,Object.assign({},o,{children:t.jsx(r,o)})):r(o)}export{S as default};
--- assets/LinkList.docs-Kt9FefJU.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as i,P as m,C as s}from"./chunk-HLWAVYOI-WSDF--JV.js";import{L as p}from"./LinkList.stories-CIc03uch.js";import{useMDXComponents as r}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DP1ccv_d.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./index-B0KGBP-1.js";import"./clsx-Du1eRyZV.js";import"./IconChevronRight-DyYvLEvC.js";import"./createReactComponent-D_5DJZVZ.js";function e(o){const n=Object.assign({h1:"h1",p:"p",a:"a",h2:"h2"},r(),o.components);return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:p}),`
-`,t.jsx(n.h1,{id:"link-list-component-react-implementation",children:"Link list Component React implementation"}),`
-`,t.jsxs(n.p,{children:["This documentation page shows the implementation of the Link list component in ",t.jsx(n.a,{href:"https://react.dev/",target:"_blank",rel:"nofollow noopener noreferrer",children:"React"}),"."]}),`
-`,t.jsx(n.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
-`,t.jsx(m,{}),`
-`,t.jsx(s,{})]})}function R(o={}){const{wrapper:n}=Object.assign({},r(),o.components);return n?t.jsx(n,Object.assign({},o,{children:t.jsx(e,o)})):e(o)}export{R as default};

53e886eaf3ac353624050bd1cb8c1631fdd7c703
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-07-15 13:29:38 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@e8e5eb95e43d7724e57530d6ea019986ad122931 🚀

---
+++ assets/Header.docs-3g60Ihmx.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as i,P as e,C as p}from"./chunk-HLWAVYOI-Bq4nCCnW.js";import{H as s}from"./Header.stories-B8WvDJmX.js";import{useMDXComponents as m}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BbfLc_-e.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./Header-kNPk6QwL.js";/* empty css                  */import"./clsx-B-dksMZM.js";import"./server.browser-rAMDWieH.js";import"./MobileMenu-h3Px0CM9.js";import"./index-3qeWHGzc.js";import"./clsx-Du1eRyZV.js";import"./index.min-DmRGC2ha.js";import"./createReactComponent-D_5DJZVZ.js";import"./IconExternalLink-DvrFVVe4.js";import"./HtmlTransformer-CIQzRuZl.js";function n(o){const r=Object.assign({h1:"h1"},m(),o.components);return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(r.h1,{id:"header-component-implementation-in-htmlcss",children:"Header Component implementation in HTML/CSS"}),`
+`,t.jsx(e,{}),`
+`,t.jsx(p,{})]})}function R(o={}){const{wrapper:r}=Object.assign({},m(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n(o)}export{R as default};
--- assets/Header.stories-7mbcDcD-.js
+++
@@ -1,8 +0,0 @@
-import{H as o}from"./Header-DDUNQpgg.js";import{H as n}from"./HtmlTransformer-CIQzRuZl.js";const s={title:"Components/Header/Html Implementation",id:"html-header",component:o,argTypes:{state:{name:"State",description:"Simulation of the state of the header items.",control:{type:"select"},options:["default","active","hover","focus-visible"]},expanded:{name:"Aria expanded",description:"Simulation of the aria-expanded attribute of all items in the header which can expand.",control:{type:"select"},options:["false","true"]}},args:{state:"default",expanded:"false"},parameters:{status:{type:"BETA"},docs:{source:{transform:n}}}},e={name:"Header"};var t,a,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  name: 'Header'
-  //   args: {
-  //     href: 'https://example.com',
-  //     title: 'Link',
-  //     children: 'Link',
-  //   },
-}`,...(r=(a=e.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const i=["Default"],l=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:i,default:s},Symbol.toStringTag,{value:"Module"}));export{l as H};

cdbc86abba56bf0318507702517a7634e88d9f57
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-07-29 14:07:38 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@9b24a015152cf78ba6207bbe2f2861d25e486638 🚀

---
+++ assets/ButtonGroup.stories-Dqt7bcsJ.js
@@ -0,0 +1,18 @@
+import{j as a}from"./jsx-runtime-CKrituN3.js";import{a as g}from"./_ButtonGroup-D3oNHoEZ.js";import{B as t}from"./index.esm-Dshv0I1Y.js";import{B as b}from"./index-C7n-SlZC.js";import{s as B,r as k,p as y}from"./server.browser-rAMDWieH.js";import{I as x}from"./IconExternalLink-DvrFVVe4.js";const j={title:"Components/Button Group/Html Implementation",id:"css-button-group",component:b,argTypes:g,args:{},parameters:{status:{type:"BETA"},docs:{source:{transform:(f,n)=>{var s,p;const o=typeof n.component=="function"?n.component:typeof((s=n.component)==null?void 0:s.render)=="function"?(p=n.component)==null?void 0:p.render:null;return o?B.format(k(o(n.args)),{parser:"babel",plugins:[y]}):f}}}}},e={args:{children:[a.jsx(t,{appearance:"primary-action-button",children:"Opslaan en doorgaan"},"1"),a.jsx(t,{appearance:"secondary-action-button",children:"Terug"},"2")]}},r={args:{children:[a.jsxs(t,{appearance:"primary-action-button",iconBeginning:"Tabler example",children:["Afspraak maken",a.jsx(x,{stroke:2})]},"1"),a.jsx(t,{appearance:"subtle-button",children:"Annuleren"},"2")]}};var c,u,i;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key="1" appearance="primary-action-button">
+        Opslaan en doorgaan
+      </Button>, <Button key="2" appearance="secondary-action-button">
+        Terug
+      </Button>]
+  }
+}`,...(i=(u=e.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var m,l,d;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: [<Button key="1" appearance="primary-action-button" iconBeginning="Tabler example">
+        Afspraak maken
+        <IconExternalLink stroke={2} />
+      </Button>, <Button key="2" appearance="subtle-button">
+        Annuleren
+      </Button>]
+  }
+}`,...(d=(l=r.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const h=["Default","DefaultVariant"],D=Object.freeze(Object.defineProperty({__proto__:null,Default:e,DefaultVariant:r,__namedExportsOrder:h,default:j},Symbol.toStringTag,{value:"Module"}));export{D as B};
---
+++ assets/LinkList.stories-CT4rVWLW.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{f as i}from"./index-C7n-SlZC.js";import{I as o}from"./IconChevronRight-DyYvLEvC.js";const a={},c={title:"Components/Link list/React Implementation",id:"react-link-list",component:i,argTypes:a,args:{},parameters:{status:{type:"BETA"}}},n={name:"Link list",args:{icon:()=>e.jsx("span",{className:"utrecht-icon",children:e.jsx(o,{})}),links:[{children:"Link 1",href:"#"},{children:"Link 2",href:"#"},{children:"Link 3",href:"#"}]}};var r,t,s;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Link list',
+  args: {
+    icon: () => <span className="utrecht-icon">
+        <IconChevronRight />
+      </span>,
+    links: [{
+      children: 'Link 1',
+      href: '#'
+    }, {
+      children: 'Link 2',
+      href: '#'
+    }, {
+      children: 'Link 3',
+      href: '#'
+    }]
+  }
+}`,...(s=(t=n.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const l=["Default"],d=Object.freeze(Object.defineProperty({__proto__:null,Default:n,__namedExportsOrder:l,default:c},Symbol.toStringTag,{value:"Module"}));export{d as L};

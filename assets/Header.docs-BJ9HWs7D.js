aec2385059512ad5caf9d9eb2e69d464b4f15bb5
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-07-29 14:29:12 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@706ad6112fdc0b607e6a91378e30b3aafc9e737f 🚀

--- assets/Header.docs-BJ9HWs7D.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as i,P as e,C as p}from"./chunk-HLWAVYOI-BZCOj92O.js";import{H as s}from"./Header.stories-BSF1wv3W.js";import{useMDXComponents as m}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-D8bXVoZB.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./Header-DkvBIRTA.js";/* empty css                  */import"./clsx-B-dksMZM.js";import"./server.browser-rAMDWieH.js";import"./MobileMenu-ZKAYrV9v.js";import"./index-C7n-SlZC.js";import"./clsx-Du1eRyZV.js";import"./index.min-ClCYwMb6.js";import"./createReactComponent-D_5DJZVZ.js";import"./IconExternalLink-DvrFVVe4.js";import"./HtmlTransformer-CIQzRuZl.js";function n(o){const r=Object.assign({h1:"h1"},m(),o.components);return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(r.h1,{id:"header-component-implementation-in-htmlcss",children:"Header Component implementation in HTML/CSS"}),`
-`,t.jsx(e,{}),`
-`,t.jsx(p,{})]})}function R(o={}){const{wrapper:r}=Object.assign({},m(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n(o)}export{R as default};

a01f6c177e144406ad986e8f33efe70e3f95e1c3
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-08-27 08:42:57 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@5c7d647b2fb5dab0789c4e700d82eef3e083a28e 🚀

---
+++ assets/ButtonGroup.docs-C0BgcGYg.js
@@ -0,0 +1,30 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as i,P as s,C as m,e as p}from"./chunk-HLWAVYOI-kfdsOaSl.js";import{B as a}from"./ButtonGroup.stories-vt14VQ95.js";import{useMDXComponents as r}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-IdfuPpnD.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./_ButtonGroup-D3oNHoEZ.js";import"./index.esm-Dshv0I1Y.js";import"./clsx-Du1eRyZV.js";import"./index-BOnBFsZr.js";import"./IconExternalLink-DvrFVVe4.js";import"./createReactComponent-D_5DJZVZ.js";function e(o){const n=Object.assign({h1:"h1",p:"p",a:"a",h2:"h2"},r(),o.components);return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:a}),`
+`,t.jsx(n.h1,{id:"button-component-react-implementation",children:"Button Component React implementation"}),`
+`,t.jsxs(n.p,{children:["This documentation page shows the implementation of the Button component in ",t.jsx(n.a,{href:"https://react.dev/",target:"_blank",rel:"nofollow noopener noreferrer",children:"React"}),"."]}),`
+`,t.jsx(n.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,t.jsx(s,{}),`
+`,t.jsx(m,{}),`
+`,t.jsx(n.h2,{id:"example-implementation",children:"Example implementation"}),`
+`,t.jsx(p,{language:"jsx",code:`
+// global
+import '@gemeentenijmegen/design-tokens/dist/index.css';
+import '@gemeentenijmegen/font/dist/index.css';
+
+// specific for ButtonGroup component
+import { ButtonGroup, Button } from "@utrecht/component-library-react";
+import '@utrecht/components/button/css/index.scss';
+import '@utrecht/components/button-group/css/index.scss';
+
+export const MyPage = () => (
+    <div className="nijmegen-theme">
+      <ButtonGroup>
+        <Button appearance="primary-action-button">Opslaan en doorgaan</Button>
+        <Button appearance="secondary-action-button">Terug</Button>
+      </ButtonGroup>
+    </div>
+);
+`}),`
+`,t.jsx(n.h2,{id:"icon-use",children:"Icon use"}),`
+`,t.jsxs(n.p,{children:["Please read the ",t.jsx(n.a,{href:"/?path=/docs/foundations-icons-developer-documentation--docs",children:"Icon developer documentation"})," if you want to use icons inside the buttons."]})]})}function R(o={}){const{wrapper:n}=Object.assign({},r(),o.components);return n?t.jsx(n,Object.assign({},o,{children:t.jsx(e,o)})):e(o)}export{R as default};
---
+++ assets/Card.stories-CCmREevz.js
@@ -0,0 +1,75 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{s as q,r as T,p as H}from"./server.browser-rAMDWieH.js";import{H as A,P as f}from"./index-BOnBFsZr.js";/* empty css                  */import{c as D}from"./clsx-B-dksMZM.js";import{I as z}from"./IconArrowRight-pMXm72se.js";import{I as g}from"./IconCalendarEvent-3BPlFUi5.js";import{I as w}from"./IconClock-Droxfkbk.js";import{c as R}from"./createReactComponent-D_5DJZVZ.js";/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var B=R("outline","map-pin","IconMapPin",[["path",{d:"M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0",key:"svg-0"}],["path",{d:"M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z",key:"svg-1"}]]);const L={title:{name:"Title",control:"text",table:{defaultValue:""}},text:{name:"Text",control:"text",table:{defaultValue:""}},image:{name:"Image",control:"text",table:{defaultValue:""}},image_alt:{name:"Image alt",control:"text",table:{defaultValue:""}},date:{name:"Date",control:"text",table:{defaultValue:""}},meta:{name:"Meta",control:{type:"object"},table:{defaultValue:""}},state:{name:"State",description:"State of the card, in the example code a state class is set for simulating the state appearance. When implementing this component it is not needed to set a state class, the state will be set by the browser and the styles will automatic apply.",control:{type:"select"},options:["default","active","hover","focus","focus-visible"]}},p=({title:m="",text:a="",meta:n=[],image:t="",image_alt:l="",date:u="",state:d=""})=>e.jsxs("div",{className:D("nijmegen-card",{"nijmegen-card--active":d==="active","nijmegen-card--hover":d==="hover","nijmegen-card--focus":d==="focus","nijmegen-card--focus-visible":d==="focus-visible"}),children:[e.jsxs("div",{className:"nijmegen-card__inner",children:[e.jsxs("div",{className:"nijmegen-card__content",children:[e.jsx(A,{children:e.jsx("a",{className:"nijmegen-card__link",href:"/",children:m})}),(!n||n.length===0)&&a&&e.jsx(f,{children:a})]}),n.length>0&&e.jsxs(e.Fragment,{children:[e.jsx("ul",{className:"nijmegen-card__meta",children:n.map((h,k)=>e.jsxs("li",{className:"nijmegen-card__meta-item",children:[h.icon,h.children]},k))}),a&&e.jsx(f,{children:a})]})]}),t&&e.jsx("figure",{className:"nijmegen-card__figure",children:e.jsx("img",{loading:"lazy",src:t,alt:l})}),e.jsxs("div",{className:"nijmegen-card__footer",children:[e.jsx(z,{}),u&&e.jsxs("div",{className:"nijmegen-card__meta-item",children:[e.jsx(g,{}),e.jsx("span",{children:u})]})]})]});try{p.displayName="CardStory",p.__docgenInfo={description:"",displayName:"CardStory",props:{title:{defaultValue:{value:""},description:"",name:"title",required:!1,type:{name:"string"}},text:{defaultValue:{value:""},description:"",name:"text",required:!1,type:{name:"string"}},meta:{defaultValue:{value:"[] as { children: string; icon: any }[]"},description:"",name:"meta",required:!1,type:{name:"{ children: string; icon: any; }[]"}},image:{defaultValue:{value:""},description:"",name:"image",required:!1,type:{name:"string"}},image_alt:{defaultValue:{value:""},description:"",name:"image_alt",required:!1,type:{name:"string"}},date:{defaultValue:{value:""},description:"",name:"date",required:!1,type:{name:"string"}},state:{defaultValue:{value:""},description:"",name:"state",required:!1,type:{name:"string"}}}}}catch{}const F={title:"Components/Card/Html Implementation",id:"html-card",argTypes:L,component:p,args:{},parameters:{status:{type:"BETA"},docs:{source:{transform:(m,a)=>{var t,l;const n=typeof a.component=="function"?a.component:typeof((t=a.component)==null?void 0:t.render)=="function"?(l=a.component)==null?void 0:l.render:null;return n?q.format(T(n(a.args)),{parser:"babel",plugins:[H]}):m}}}}},r={name:"Card",args:{title:"Ondernemers opgelet",text:"Ondernemers en bedrijven uit Nijmegen opgelet!",image:"https://picsum.photos/640/360/",image_alt:"Image description",date:"7 februari 2024",meta:[{children:"Meta 1",icon:e.jsx(g,{})},{children:"Meta 2",icon:e.jsx(w,{})},{children:"Meta 3",icon:e.jsx(B,{})}]}},s={name:"Card news",args:{title:"Ondernemers opgelet",text:"Ondernemers en bedrijven uit Nijmegen opgelet!",image:"https://picsum.photos/640/360/",image_alt:"Image description",date:"7 februari 2024"}},i={name:"Card event",args:{title:"Bevrijdingsdag 2025",text:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod",image:"https://picsum.photos/640/360/",image_alt:"Image description",meta:[{children:"Meta 1",icon:e.jsx(g,{})},{children:"Meta 2",icon:e.jsx(w,{})},{children:"Meta 3",icon:e.jsx(B,{})}]}},o={name:"Card persona",args:{title:"Hubert Bruls",text:"Burgemeester",image:"https://picsum.photos/640/360/",image_alt:"Image description"}},c={name:"Card no image",args:{title:"Ondernemers opgelet",text:"Ondernemers en bedrijven uit Nijmegen opgelet!",image:"",image_alt:"",date:"7 februari 2024"}};var j,_,x;r.parameters={...r.parameters,docs:{...(j=r.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  name: 'Card',
+  args: {
+    title: 'Ondernemers opgelet',
+    text: 'Ondernemers en bedrijven uit Nijmegen opgelet!',
+    image: 'https://picsum.photos/640/360/',
+    image_alt: 'Image description',
+    date: '7 februari 2024',
+    meta: ([{
+      children: 'Meta 1',
+      icon: <IconCalendarEvent />
+    }, {
+      children: 'Meta 2',
+      icon: <IconClock />
+    }, {
+      children: 'Meta 3',
+      icon: <IconMapPin />
+    }] as Array<{
+      children: string;
+      icon: any;
+    }>)
+  }
+}`,...(x=(_=r.parameters)==null?void 0:_.docs)==null?void 0:x.source}}};var v,b,y;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  name: 'Card news',
+  args: {
+    title: 'Ondernemers opgelet',
+    text: 'Ondernemers en bedrijven uit Nijmegen opgelet!',
+    image: 'https://picsum.photos/640/360/',
+    image_alt: 'Image description',
+    date: '7 februari 2024'
+  }
+}`,...(y=(b=s.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var I,N,M;i.parameters={...i.parameters,docs:{...(I=i.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  name: 'Card event',
+  args: {
+    title: 'Bevrijdingsdag 2025',
+    text: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    image: 'https://picsum.photos/640/360/',
+    image_alt: 'Image description',
+    meta: ([{
+      children: 'Meta 1',
+      icon: <IconCalendarEvent />
+    }, {
+      children: 'Meta 2',
+      icon: <IconClock />
+    }, {
+      children: 'Meta 3',
+      icon: <IconMapPin />
+    }] as Array<{
+      children: string;
+      icon: any;
+    }>)
+  }
+}`,...(M=(N=i.parameters)==null?void 0:N.docs)==null?void 0:M.source}}};var C,O,S;o.parameters={...o.parameters,docs:{...(C=o.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  name: 'Card persona',
+  args: {
+    title: 'Hubert Bruls',
+    text: 'Burgemeester',
+    image: 'https://picsum.photos/640/360/',
+    image_alt: 'Image description'
+  }
+}`,...(S=(O=o.parameters)==null?void 0:O.docs)==null?void 0:S.source}}};var V,E,P;c.parameters={...c.parameters,docs:{...(V=c.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  name: 'Card no image',
+  args: {
+    title: 'Ondernemers opgelet',
+    text: 'Ondernemers en bedrijven uit Nijmegen opgelet!',
+    image: '',
+    image_alt: '',
+    date: '7 februari 2024'
+  }
+}`,...(P=(E=c.parameters)==null?void 0:E.docs)==null?void 0:P.source}}};const W=["Default","News","Event","Persona","NoImage"],ee=Object.freeze(Object.defineProperty({__proto__:null,Default:r,Event:i,News:s,NoImage:c,Persona:o,__namedExportsOrder:W,default:F},Symbol.toStringTag,{value:"Module"}));export{ee as C,r as D,i as E,c as N,o as P};

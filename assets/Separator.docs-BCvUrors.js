d4c7ea6387fe81d1b3c4c74fde33bcbbc9a518b8
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-08-27 09:12:52 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@dceb3cb4bed52d6ae2205387844494498ed749f1 🚀

---
+++ assets/Separator.docs-BCvUrors.js
@@ -0,0 +1,8 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as m,P as a,C as i}from"./chunk-HLWAVYOI-lzILvU3L.js";import{S as s}from"./Separator.stories-Cz3xeTEf.js";import{useMDXComponents as r}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BrpWY5j_.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./index-BOnBFsZr.js";import"./clsx-Du1eRyZV.js";function e(n){const o=Object.assign({h1:"h1",p:"p",a:"a",h2:"h2"},r(),n.components);return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(m,{of:s}),`
+`,t.jsx(o.h1,{id:"separator-component-react-implementation",children:"Separator Component React implementation"}),`
+`,t.jsxs(o.p,{children:["This documentation page shows the implementation of the Separator component in ",t.jsx(o.a,{href:"https://react.dev/",target:"_blank",rel:"nofollow noopener noreferrer",children:"React"}),"."]}),`
+`,t.jsx(o.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i,{})]})}function O(n={}){const{wrapper:o}=Object.assign({},r(),n.components);return o?t.jsx(o,Object.assign({},n,{children:t.jsx(e,n)})):e(n)}export{O as default};

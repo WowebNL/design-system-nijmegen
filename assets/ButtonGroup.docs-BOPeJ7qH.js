159308dbfa6e858b91906e2bb58e13c0505aa926
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-09-04 08:07:40 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@d327ae9eb828b103fdee4dfae46b5ff46003a3a1 🚀

---
+++ assets/ButtonGroup.docs-BOPeJ7qH.js
@@ -0,0 +1,27 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as r,P as i,C as m,e as p}from"./chunk-HLWAVYOI-Gip50_L9.js";import{B as c}from"./ButtonGroup.stories-DbW2UnIK.js";import{useMDXComponents as s}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-mfrdEfr0.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-iH-BfeAc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./_ButtonGroup-D3oNHoEZ.js";import"./index.esm-Dshv0I1Y.js";import"./clsx-Du1eRyZV.js";import"./index-BspXdmHl.js";import"./server.browser-BOeajTT4.js";import"./IconExternalLink-DvrFVVe4.js";import"./createReactComponent-D_5DJZVZ.js";const u=`<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <title>Button group example</title>
+    <!-- global -->
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/document-css@1.5.0/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/design-tokens@latest/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/font@latest/dist/index.css" />
+    <!-- component specific -->
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/button-css@2.3.0/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/button-group-css@1.4.0/dist/index.css" />
+  </head>
+  <body class="utrecht-document nijmegen-theme">
+    <p role="group" class="utrecht-button-group utrecht-button-group">
+        <button class="utrecht-button utrecht-button--primary-action" type="button">Opslaan en doorgaan</button>
+        <button class="utrecht-button utrecht-button--secondary-action" type="button">Terug</button>
+    </p>
+  </body>
+</html>`;function o(n){const e=Object.assign({h1:"h1",h2:"h2"},s(),n.components);return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(r,{of:c}),`
+`,t.jsx(e.h1,{id:"button-group-component-implementation-in-htmlcss",children:"Button Group Component implementation in HTML/CSS"}),`
+`,t.jsx(e.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,t.jsx(i,{}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e.h2,{id:"example-implementation",children:"Example implementation"}),`
+`,t.jsx(p,{language:"html",code:u})]})}function L(n={}){const{wrapper:e}=Object.assign({},s(),n.components);return e?t.jsx(e,Object.assign({},n,{children:t.jsx(o,n)})):o(n)}export{L as default};

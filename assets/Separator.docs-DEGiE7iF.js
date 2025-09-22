3ab8e181d17a9ed4397e06d10bf42d446c54a081
Michel-Verhoeven <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-09-22 13:17:23 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@a601a76473c509b1346be7a84b18c974f0abccef 🚀

--- assets/Separator.docs-DEGiE7iF.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as m,P as i,C as s}from"./chunk-HLWAVYOI-C7l3ThbU.js";import{S as p}from"./Separator.stories-Bk0Xb1vf.js";import{useMDXComponents as e}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BmtkS5jk.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-iH-BfeAc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./index-BspXdmHl.js";import"./clsx-Du1eRyZV.js";import"./server.browser-BOeajTT4.js";function r(o){const n=Object.assign({h1:"h1",h2:"h2"},e(),o.components);return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(m,{of:p}),`
-`,t.jsx(n.h1,{id:"separator-component-implementation-in-htmlcss",children:"Separator Component implementation in HTML/CSS"}),`
-`,t.jsx(n.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
-`,t.jsx(i,{}),`
-`,t.jsx(s,{})]})}function y(o={}){const{wrapper:n}=Object.assign({},e(),o.components);return n?t.jsx(n,Object.assign({},o,{children:t.jsx(r,o)})):r(o)}export{y as default};

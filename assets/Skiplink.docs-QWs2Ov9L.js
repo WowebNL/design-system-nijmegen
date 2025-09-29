1ad5f633588b30ffe969ad6a6d6fd420911acde4
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-09-29 09:51:41 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@c83312087a2438cc1a866b7fe78c3190a3180413 🚀

---
+++ assets/Skiplink.docs-QWs2Ov9L.js
@@ -0,0 +1,25 @@
+import{j as n}from"./jsx-runtime-CKrituN3.js";import{M as o,P as m,C as r,e as p}from"./chunk-HLWAVYOI-oMfa3suu.js";import{S as l}from"./Skiplink.stories-nhiCzxID.js";import{useMDXComponents as i}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-8DV53hpr.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-ToFVweKJ.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";/* empty css                  */import"./clsx-B-dksMZM.js";const a=`<!doctype html>
+<html lang="nl">
+  <head>
+    <title>Skip link example</title>
+    <!-- global -->
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/document-css@1.5.0/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/design-tokens@latest/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/font@latest/dist/index.css" />
+    <!-- component specific -->
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/components-css@latest/dist/index.min.css" />
+  </head>
+
+  <body class="utrecht-document nijmegen-theme">
+    <a href="#main" class="nijmegen-skip-link">Ga naar hoofdinhoud</a>;
+  </body>
+</html>
+`;function s(e){const t=Object.assign({h1:"h1",h2:"h2"},i(),e.components);return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(t.h1,{id:"skip-link-component-implementation-in-htmlcss",children:"Skip link Component implementation in HTML/CSS"}),`
+`,n.jsx(t.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,n.jsx(m,{}),`
+`,n.jsx(r,{}),`
+`,n.jsx(t.h2,{id:"example-implementation",children:"Example implementation"}),`
+`,n.jsx(p,{language:"html",code:a})]})}function E(e={}){const{wrapper:t}=Object.assign({},i(),e.components);return t?n.jsx(t,Object.assign({},e,{children:n.jsx(s,e)})):s(e)}export{E as default};

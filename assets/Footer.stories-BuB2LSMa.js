92c4e96bb6a56b463820251adbb17a25273387a4
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-09-29 13:03:20 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@fe3bb696ca0d69d92bb7405655a30efc0b7c1859 🚀

---
+++ assets/Footer.stories-BuB2LSMa.js
@@ -0,0 +1,49 @@
+import{s as N,r as b,p as y}from"./server.browser-rAMDWieH.js";import{j as e}from"./jsx-runtime-CKrituN3.js";import{e as t,P as d}from"./index-CdovAc_6.js";/* empty css                  */import{c as s}from"./createReactComponent-D_5DJZVZ.js";import{I as w}from"./IconClock-Droxfkbk.js";/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var I=s("outline","brand-facebook","IconBrandFacebook",[["path",{d:"M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3",key:"svg-0"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var M=s("outline","brand-instagram","IconBrandInstagram",[["path",{d:"M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z",key:"svg-0"}],["path",{d:"M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0",key:"svg-1"}],["path",{d:"M16.5 7.5l0 .01",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var S=s("outline","brand-linkedin","IconBrandLinkedin",[["path",{d:"M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z",key:"svg-0"}],["path",{d:"M8 11l0 5",key:"svg-1"}],["path",{d:"M8 8l0 .01",key:"svg-2"}],["path",{d:"M12 16l0 -5",key:"svg-3"}],["path",{d:"M16 16v-3a2 2 0 0 0 -4 0",key:"svg-4"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var B=s("outline","brand-x","IconBrandX",[["path",{d:"M4 4l11.733 16h4.267l-11.733 -16z",key:"svg-0"}],["path",{d:"M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772",key:"svg-1"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var F=s("outline","phone","IconPhone",[["path",{d:"M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2",key:"svg-0"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var P=s("outline","send","IconSend",[["path",{d:"M10 14l11 -11",key:"svg-0"}],["path",{d:"M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5",key:"svg-1"}]]);const z={show_main:{name:"Show main footer",control:{type:"boolean"},table:{disable:!0}},show_sub:{name:"Show main footer",control:{type:"boolean"},table:{disable:!0}}},o=({show_main:r=!0,show_sub:n=!0})=>e.jsx("footer",{className:"nijmegen-footer",children:e.jsxs("div",{className:"nijmegen-footer__container",children:[r&&e.jsxs("div",{className:"nijmegen-footer__main",children:[e.jsx("div",{className:"nijmegen-footer__col",children:e.jsxs("div",{className:"nijmegen-footer__block",children:[e.jsx(t,{appearance:"utrecht-heading-4",level:2,children:"Meer weten"}),e.jsxs("ul",{className:"nijmegen-link-list",children:[e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsx("a",{className:"nijmegen-link-list__link",href:"#",children:e.jsx("span",{children:"Over ons"})})}),e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsx("a",{className:"nijmegen-link-list__link",href:"#",children:e.jsx("span",{children:"Werken bij"})})}),e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsx("a",{className:"nijmegen-link-list__link",href:"#",children:e.jsx("span",{children:"Into Nijmegen"})})}),e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsx("a",{className:"nijmegen-link-list__link",href:"#",children:e.jsx("span",{children:"Onderzoek en cijfers"})})})]})]})}),e.jsx("div",{className:"nijmegen-footer__col",children:e.jsxs("div",{className:"nijmegen-footer__block",children:[e.jsx(t,{appearance:"utrecht-heading-4",level:2,children:"Contact"}),e.jsx(d,{children:"Heeft u een vraag en vindt u het antwoord niet op onze website? Neem dan gerust contact met ons op."}),e.jsxs("ul",{className:"nijmegen-link-list",children:[e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsxs("a",{className:"nijmegen-link-list__link nijmegen-link-list__link--no-chevron",href:"#",title:"Telefoonnummer Gemeente Nijmegen",children:[e.jsx(F,{}),e.jsx("span",{children:"Bel 14 024"})]})}),e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsxs("a",{className:"nijmegen-link-list__link nijmegen-link-list__link--no-chevron",href:"#",children:[e.jsx(P,{}),e.jsx("span",{children:"Contactformulier"})]})}),e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsxs("a",{className:"nijmegen-link-list__link nijmegen-link-list__link--no-chevron",href:"#",children:[e.jsx(w,{}),e.jsx("span",{children:"Contactgegevens en openingstijden"})]})})]})]})}),e.jsxs("div",{className:"nijmegen-footer__col",children:[e.jsxs("div",{className:"nijmegen-footer__block",children:[e.jsx(t,{appearance:"utrecht-heading-4",level:2,children:"Nieuwsbrief"}),e.jsx("ul",{className:"nijmegen-link-list",children:e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsx("a",{className:"nijmegen-link-list__link",href:"#",children:e.jsx("span",{children:"Inschrijven"})})})})]}),e.jsxs("div",{className:"nijmegen-footer__block",children:[e.jsx(t,{appearance:"utrecht-heading-4",level:2,children:"Volg gemeente Nijmegen"}),e.jsxs("ul",{className:"nijmegen-link-list",children:[e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsxs("a",{className:"nijmegen-link-list__link nijmegen-link-list__link--no-chevron",href:"#",children:[e.jsx(I,{}),e.jsx("span",{children:"Facebook"})]})}),e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsxs("a",{className:"nijmegen-link-list__link nijmegen-link-list__link--no-chevron",href:"#",children:[e.jsx(B,{}),e.jsx("span",{children:"X"})]})}),e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsxs("a",{className:"nijmegen-link-list__link nijmegen-link-list__link--no-chevron",href:"#",children:[e.jsx(M,{}),e.jsx("span",{children:"Instagram"})]})}),e.jsx("li",{className:"nijmegen-link-list__item",children:e.jsxs("a",{className:"nijmegen-link-list__link nijmegen-link-list__link--no-chevron",href:"#",children:[e.jsx(S,{}),e.jsx("span",{children:"LinkedIn"})]})})]})]})]})]}),n&&e.jsxs("div",{className:"nijmegen-footer__sub",children:[e.jsx(d,{children:"© Gemeente Nijmegen 2025"}),e.jsxs("ul",{children:[e.jsx("li",{children:e.jsx("a",{href:"",className:"utrecht-link utrecht-link--html-a",children:"Toegankelijkheid"})}),e.jsx("li",{children:e.jsx("a",{href:"",className:"utrecht-link utrecht-link--html-a",children:"Privacy"})}),e.jsx("li",{children:e.jsx("a",{href:"",className:"utrecht-link utrecht-link--html-a",children:"Cookies"})}),e.jsx("li",{children:e.jsx("a",{href:"",className:"utrecht-link utrecht-link--html-a",children:"Proclaimer"})}),e.jsx("li",{children:e.jsx("a",{href:"",className:"utrecht-link utrecht-link--html-a",children:"Sitemap"})})]})]})]})});try{o.displayName="FooterStory",o.__docgenInfo={description:"",displayName:"FooterStory",props:{show_main:{defaultValue:{value:"true"},description:"",name:"show_main",required:!1,type:{name:"boolean"}},show_sub:{defaultValue:{value:"true"},description:"",name:"show_sub",required:!1,type:{name:"boolean"}}}}}catch{}const T={title:"Components/Footer/Html Implementation",id:"html-footer",argTypes:z,component:o,args:{},parameters:{status:{type:"BETA"},docs:{source:{transform:(r,n)=>{var m,h;const c=typeof n.component=="function"?n.component:typeof((m=n.component)==null?void 0:m.render)=="function"?(h=n.component)==null?void 0:h.render:null;return c?N.format(b(c(n.args)),{parser:"babel",plugins:[y]}):r}}}}},i={name:"Footer",args:{show_main:!0,show_sub:!0}},a={name:"Main footer",args:{show_main:!0,show_sub:!1}},l={name:"Sub footer",args:{show_main:!1,show_sub:!0}};var j,_,g;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  name: 'Footer',
+  args: {
+    show_main: true,
+    show_sub: true
+  }
+}`,...(g=(_=i.parameters)==null?void 0:_.docs)==null?void 0:g.source}}};var k,p,u;a.parameters={...a.parameters,docs:{...(k=a.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  name: 'Main footer',
+  args: {
+    show_main: true,
+    show_sub: false
+  }
+}`,...(u=(p=a.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var x,f,v;l.parameters={...l.parameters,docs:{...(x=l.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  name: 'Sub footer',
+  args: {
+    show_main: false,
+    show_sub: true
+  }
+}`,...(v=(f=l.parameters)==null?void 0:f.docs)==null?void 0:v.source}}};const O=["Default","Main","Sub"],q=Object.freeze(Object.defineProperty({__proto__:null,Default:i,Main:a,Sub:l,__namedExportsOrder:O,default:T},Symbol.toStringTag,{value:"Module"}));export{q as F};

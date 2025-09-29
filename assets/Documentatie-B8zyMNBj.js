8a249b41c378d92ff32d2615d87b374e2dbf722a
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-09-29 15:07:11 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@df835dcc20df847071d76732de5859a676bc6883 🚀

---
+++ assets/Documentatie-B8zyMNBj.js
@@ -0,0 +1,11 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{b as o}from"./chunk-HLWAVYOI-u1aNWk03.js";import{D as r,F as a,V as c}from"./Skiplink.stories-nhiCzxID.js";import{useMDXComponents as s}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B1bkAkvG.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-ToFVweKJ.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";/* empty css                  */import"./clsx-B-dksMZM.js";function i(t){const n=Object.assign({h1:"h1",h2:"h2",p:"p",table:"table",thead:"thead",tr:"tr",th:"th",tbody:"tbody",td:"td",a:"a",h3:"h3"},s(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{id:"documentatie-skip-link-component",children:"Documentatie skip link component"}),`
+`,e.jsx(n.h2,{id:"component",children:"Component"}),`
+`,e.jsx(n.p,{children:"Dit component is op maat ontwikkeld op basis van het ontwerp voor de Gemeente Nijmegen."}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{children:"Link"}),e.jsx(n.th,{children:"Omschrijving"})]})}),e.jsx(n.tbody,{children:e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.a,{href:"https://www.figma.com/design/g3AieWAFD3NtkbDDTTFC30/Novio---Nijmegen-Design-System?node-id=10621-8554",target:"_blank",rel:"nofollow noopener noreferrer",children:"Figma gemeente Nijmegen"})}),e.jsx(n.td,{children:"Design documentatie van dit component."})]})})]}),`
+`,e.jsx(n.h2,{id:"variaties",children:"Variaties"}),`
+`,e.jsx(n.h3,{id:"default",children:"Default"}),`
+`,e.jsx(o,{of:r,sourceState:"none"}),`
+`,e.jsx(n.h3,{id:"default-focus",children:"Default focus"}),`
+`,e.jsx(o,{of:a,sourceState:"none"}),`
+`,e.jsx(n.h3,{id:"visible-on-focus",children:"Visible on focus"}),`
+`,e.jsx(o,{of:c,sourceState:"none"})]})}function y(t={}){const{wrapper:n}=Object.assign({},s(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(i,t)})):i(t)}export{y as default};

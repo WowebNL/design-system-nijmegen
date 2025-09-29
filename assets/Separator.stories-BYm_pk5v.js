348e3c9be0ca46154fe12e0aa84ff60a673115b8
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-09-29 14:51:07 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@7f39c2e37dadea7a2152d9b817ca9cad6bfe7f74 🚀

---
+++ assets/Separator.stories-BYm_pk5v.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{S as m}from"./index-CdovAc_6.js";const l=()=>t.jsx("div",{role:"separator","aria-hidden":"true",className:"utrecht-separator"}),d=({element:c})=>c==="div"?t.jsx(l,{}):t.jsx(m,{}),u={title:"Components/Separator/React Implementation",id:"react-separator",component:d,argTypes:{element:{control:{type:"select"},options:["div","separator"]}},args:{element:"separator"},parameters:{status:{type:"BETA"}}},e={name:"Separator"},r={args:{element:"div"},parameters:{docs:{description:{story:"Markup with a `<div>` element, with additional attributes to give the correct ARIA role. Styling via `utrecht-separator` class name."}}}};var a,o,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: 'Separator'
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var n,p,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    element: 'div'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Markup with a \`<div>\` element, with additional attributes to give the correct ARIA role. Styling via \`utrecht-separator\` class name.'
+      }
+    }
+  }
+}`,...(i=(p=r.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};const S=["Default","SeparatorDivSupport"],h=Object.freeze(Object.defineProperty({__proto__:null,Default:e,SeparatorDivSupport:r,__namedExportsOrder:S,default:u},Symbol.toStringTag,{value:"Module"}));export{h as S};

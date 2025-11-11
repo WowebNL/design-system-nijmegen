85ab99a9e04d6a8d67d38b47437cc291106e8d49
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-11-11 14:25:42 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@2fc247d6bec5da86873cdc5b9ca76613ffc25d7f 🚀

---
+++ assets/Listbox.docs-CN_AiGl7.js
@@ -0,0 +1,37 @@
+import{j as n}from"./jsx-runtime-CKrituN3.js";import{M as s,P as m,b as l,e as r}from"./chunk-HLWAVYOI-Debet4NX.js";import{L as p}from"./Listbox.stories-D3pKRjL7.js";import{useMDXComponents as o}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-D4V4lNgB.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";/* empty css                  */import"./index-Dst2WnqY.js";import"./index-7bwtWdbn.js";const c=`<!doctype html>
+<html lang="nl">
+  <head>
+    <title>Card example</title>
+    <!-- global -->
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/document-css@1.5.0/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/design-tokens@latest/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/font@latest/dist/index.css" />
+    <!-- component specific -->
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/components-css@latest/dist/index.min.css" />
+  </head>
+
+  <body class="utrecht-document nijmegen-theme">
+    <div class="nijmegen-listbox" role="listbox" tabindex="0">
+      <ul class="nijmegen-listbox__list" role="none">
+        <li class="nijmegen-listbox__item" role="option">
+          Option #1
+        </li>
+        <li class="nijmegen-listbox__item" role="option">
+          Option #2
+        </li>
+        <li class="nijmegen-listbox__item" role="option">
+          Option #3
+        </li>
+      </ul>
+    </div>
+  </body>
+</html>
+`;function i(e){const t=Object.assign({h1:"h1",h2:"h2"},o(),e.components);return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(s,{of:p}),`
+`,n.jsx(t.h1,{id:"skip-link-component-implementation-in-htmlcss",children:"Skip link Component implementation in HTML/CSS"}),`
+`,n.jsx(t.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,n.jsx(m,{}),`
+`,n.jsx(l,{}),`
+`,n.jsx(t.h2,{id:"example-implementation",children:"Example implementation"}),`
+`,n.jsx(r,{language:"html",code:c})]})}function X(e={}){const{wrapper:t}=Object.assign({},o(),e.components);return t?n.jsx(t,Object.assign({},e,{children:n.jsx(i,e)})):i(e)}export{X as default};

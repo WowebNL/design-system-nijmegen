49afee1d043d1c8ab4078dc99e011496591e1092
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-11-11 14:17:48 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@8a073a5f6bfa1ed3d34b2a81ec40c96d723bb162 🚀

---
+++ assets/Listbox.docs-BcfiSz7a.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as r,P as e,b as s}from"./chunk-HLWAVYOI-CsVHd2MX.js";import{L as p}from"./Listbox.stories-D3pKRjL7.js";import{useMDXComponents as m}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DINhb-NC.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";/* empty css                  */import"./index-Dst2WnqY.js";import"./index-7bwtWdbn.js";function i(n){const o=Object.assign({h1:"h1",h2:"h2"},m(),n.components);return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(r,{of:p}),`
+`,t.jsx(o.h1,{id:"skip-link-component-implementation-in-htmlcss",children:"Skip link Component implementation in HTML/CSS"}),`
+`,t.jsx(o.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,t.jsx(e,{}),`
+`,t.jsx(s,{})]})}function X(n={}){const{wrapper:o}=Object.assign({},m(),n.components);return o?t.jsx(o,Object.assign({},n,{children:t.jsx(i,n)})):i(n)}export{X as default};

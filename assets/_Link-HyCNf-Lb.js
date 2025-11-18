e70873e8d4393e0b7625c6959d9c89cff0ceef85
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-11-18 15:26:52 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@4d620a0bab2e527359f1764aa815e1f37e9bd81b 🚀

--- assets/_Link-HyCNf-Lb.js
+++
@@ -1,1 +0,0 @@
-const e={title:{name:"Title",control:"text"},children:{name:"Children",control:"text"},rel:{name:"rel",control:"text"},target:{name:"Target",description:"Link target",control:{type:"select"},options:["_self","_blank","_parent","_top"]},placeholder:{name:"Placeholder",description:"Show the placeholder",control:{type:"boolean"},table:{type:{summary:["boolean"]}}}};export{e as a};

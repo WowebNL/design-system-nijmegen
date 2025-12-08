fd1a2b76515e70cd41b2e8dd3e84dc5f271f8d27
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-12-08 10:08:29 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@5e044317ac10df4efc8d0571254aed03c9497eb6 🚀

---
+++ assets/Search.stories-CUO_75jT.js
@@ -0,0 +1,37 @@
+import{s as O,r as Z,p as D}from"./server.browser-rAMDWieH.js";import{a as _,b as T}from"./_Search-CrSiTXGD.js";const E={title:"Components/Search/Web Component Implementation",id:"web-component-search",argTypes:_,component:T,args:{},parameters:{status:{type:"BETA"},docs:{source:{transform:(y,e)=>{var d,l;const o=typeof e.component=="function"?e.component:typeof((d=e.component)==null?void 0:d.render)=="function"?(l=e.component)==null?void 0:l.render:null;return o?O.format(Z(o(e.args)),{parser:"babel",plugins:[D]}):y}}}}},a={name:"Search",args:{placeholder:"Zoeken",icon:!0,disabled:!1,dark:!1,readOnly:!1}},r={name:"Search disabled",args:{placeholder:"Zoeken",icon:!0,disabled:!0,dark:!1,readOnly:!1}},n={name:"Search read only",args:{placeholder:"Zoeken",icon:!0,disabled:!1,dark:!1,readOnly:!0}},s={name:"Search dark mode",args:{placeholder:"Zoeken",icon:!0,disabled:!1,dark:!0,readOnly:!1}};var c,t,p;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  name: 'Search',
+  args: {
+    placeholder: 'Zoeken',
+    icon: true,
+    disabled: false,
+    dark: false,
+    readOnly: false
+  }
+}`,...(p=(t=a.parameters)==null?void 0:t.docs)==null?void 0:p.source}}};var m,i,u;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  name: 'Search disabled',
+  args: {
+    placeholder: 'Zoeken',
+    icon: true,
+    disabled: true,
+    dark: false,
+    readOnly: false
+  }
+}`,...(u=(i=r.parameters)==null?void 0:i.docs)==null?void 0:u.source}}};var f,b,k;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  name: 'Search read only',
+  args: {
+    placeholder: 'Zoeken',
+    icon: true,
+    disabled: false,
+    dark: false,
+    readOnly: true
+  }
+}`,...(k=(b=n.parameters)==null?void 0:b.docs)==null?void 0:k.source}}};var h,S,g;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  name: 'Search dark mode',
+  args: {
+    placeholder: 'Zoeken',
+    icon: true,
+    disabled: false,
+    dark: true,
+    readOnly: false
+  }
+}`,...(g=(S=s.parameters)==null?void 0:S.docs)==null?void 0:g.source}}};const j=["Default","Disabled","ReadOnly","Dark"],R=Object.freeze(Object.defineProperty({__proto__:null,Dark:s,Default:a,Disabled:r,ReadOnly:n,__namedExportsOrder:j,default:E},Symbol.toStringTag,{value:"Module"}));export{R as S};

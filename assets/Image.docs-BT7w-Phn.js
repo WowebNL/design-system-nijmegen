8646e7be20b594898793a9303313db57f28ddd62
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2025-12-15 15:15:05 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@3741acec7176f095c75e155b229474ce34b16bb1 🚀

---
+++ assets/Image.docs-BT7w-Phn.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as m,P as o,b as r,e as p}from"./chunk-HLWAVYOI-DbTNPLu1.js";import{I as a}from"./Image.stories-CeNhb-5P.js";import{useMDXComponents as i}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-n-Xg9UA3.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";/* empty css                  */const l=`<!doctype html>
+<html lang="nl">
+  <head>
+    <title>Example</title>
+    <!-- global -->
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/document-css@1.5.0/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/design-tokens@latest/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/font@latest/dist/index.css" />
+    <!-- component specific -->
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/link-css@1.6.1/dist/index.css" />
+  </head>
+
+  <body class="utrecht-document nijmegen-theme">
+    <img
+      class="nijmegen-image nijmegen-image--aspect-16-9"
+      src="https://picsum.photos/640/360/"
+      alt="Example image description"
+      width="640"
+      loading="lazy"
+    />
+  </body>
+</html>
+`;function s(n){const t=Object.assign({h1:"h1",h2:"h2"},i(),n.components);return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(m,{of:a}),`
+`,e.jsx(t.h1,{id:"image-component-implementation-in-htmlcss",children:"Image Component implementation in HTML/CSS"}),`
+`,e.jsx(t.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,e.jsx(o,{}),`
+`,e.jsx(r,{}),`
+`,e.jsx(t.h2,{id:"example-implementation",children:"Example implementation"}),`
+`,e.jsx(p,{language:"html",code:l})]})}function O(n={}){const{wrapper:t}=Object.assign({},i(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(s,n)})):s(n)}export{O as default};

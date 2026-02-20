e91a456be63ce96c3d18bd65a9f008e610f19db6
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2026-02-20 10:43:42 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@e4ce0db9f593f03c06d0f655babb56881668bb5c 🚀

---
+++ assets/Listbox.stories-BVcJBdNq.js
@@ -0,0 +1,14 @@
+import{s as m,r as c,p as f}from"./server.browser-rAMDWieH.js";import{L as l,a as u}from"./_Listbox-ZcmgXc-A.js";const d={title:"Components/Listbox/Html Implementation",id:"html-listbox",component:l,argTypes:u,args:{},parameters:{status:{type:"BETA"},docs:{source:{transform:(i,e)=>{var n,o;const r=typeof e.component=="function"?e.component:typeof((n=e.component)==null?void 0:n.render)=="function"?(o=e.component)==null?void 0:o.render:null;return r?m.format(c(r(e.args)),{parser:"babel",plugins:[f]}):i}}}}},t={args:{items:[{text:"Option #1",href:"#"},{text:"Option #2",href:"#"},{text:"Option #3",href:"#"}]}};var a,s,p;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      text: 'Option #1',
+      href: '#'
+    }, {
+      text: 'Option #2',
+      href: '#'
+    }, {
+      text: 'Option #3',
+      href: '#'
+    }]
+  }
+}`,...(p=(s=t.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};const b=["Default"],O=Object.freeze(Object.defineProperty({__proto__:null,Default:t,__namedExportsOrder:b,default:d},Symbol.toStringTag,{value:"Module"}));export{t as D,O as L};

c74677c2bdab8fbc2d615941e50634906c5f7bd5
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2026-03-05 08:39:35 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@472cf12f4701d1ad45c5224b11849fbe6c7cbba6 🚀

--- assets/preview-CWb_qgoC.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-Rj-vnXBB.js","./chunk-HLWAVYOI-TRwraqCw.js","./iframe-BFfm4mOp.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-CnWNB3mc.js","./extends-CCbyfPlC.js","./_getPrototype-nBa_wnor.js","./index-ltOY3w3e.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as e}from"./iframe-BFfm4mOp.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-Rj-vnXBB.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};

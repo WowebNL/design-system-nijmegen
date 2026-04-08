b3296dd0bab8492efd340722d8793596958d22cd
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2026-04-08 10:25:23 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@7bfe3a026ea76e13014b276403d5a105b3c61f55 🚀

---
+++ assets/SideNavigation.stories-TQNuDhbv.js
@@ -0,0 +1,122 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{s as b,r as z,p as S}from"./server.browser-rAMDWieH.js";/* empty css                  */import{c as a}from"./createReactComponent-D_5DJZVZ.js";import{I as u}from"./IconUser-CHKGFGGR.js";/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var x=a("outline","archive","IconArchive",[["path",{d:"M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z",key:"svg-0"}],["path",{d:"M5 8v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-10",key:"svg-1"}],["path",{d:"M10 12l4 0",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var f=a("outline","building-community","IconBuildingCommunity",[["path",{d:"M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8",key:"svg-0"}],["path",{d:"M13 7l0 .01",key:"svg-1"}],["path",{d:"M17 7l0 .01",key:"svg-2"}],["path",{d:"M17 11l0 .01",key:"svg-3"}],["path",{d:"M17 15l0 .01",key:"svg-4"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var y=a("outline","currency-euro","IconCurrencyEuro",[["path",{d:"M17.2 7a6 7 0 1 0 0 10",key:"svg-0"}],["path",{d:"M13 10h-8m0 4h8",key:"svg-1"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var k=a("outline","home","IconHome",[["path",{d:"M5 12l-2 0l9 -9l9 9l-2 0",key:"svg-0"}],["path",{d:"M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7",key:"svg-1"}],["path",{d:"M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var I=a("outline","inbox","IconInbox",[["path",{d:"M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z",key:"svg-0"}],["path",{d:"M4 13h3l3 3h4l3 -3h3",key:"svg-1"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var j=a("outline","layout-grid","IconLayoutGrid",[["path",{d:"M4 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z",key:"svg-0"}],["path",{d:"M14 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z",key:"svg-1"}],["path",{d:"M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z",key:"svg-2"}],["path",{d:"M14 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z",key:"svg-3"}]]);/**
+ * @license @tabler/icons-react v3.20.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var _=a("outline","parking","IconParking",[["path",{d:"M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z",key:"svg-0"}],["path",{d:"M10 16v-8h2.667c.736 0 1.333 .895 1.333 2s-.597 2 -1.333 2h-2.667",key:"svg-1"}]]);const B={lists:{control:"object"}},h=({lists:r})=>e.jsx("nav",{className:"nijmegen-side-navigation",children:r==null?void 0:r.map((t,c)=>{var o;return e.jsx("ul",{className:"nijmegen-side-navigation__list",children:(o=t.items)==null?void 0:o.map((n,M)=>e.jsx("li",{className:"nijmegen-side-navigation__item",children:e.jsxs("a",{...n.current&&{"aria-current":"page"},className:`nijmegen-side-navigation__link${n.current?" nijmegen-side-navigation__link--current":""}`,href:n.href,children:[n.icon,e.jsx("span",{className:"nijmegen-side-navigation__link-label",children:n.text}),n.badge!==void 0&&e.jsx("span",{className:"nijmegen-number-badge",children:n.badge})]})},M))},c)})});try{h.displayName="SideNavigationStory",h.__docgenInfo={description:"",displayName:"SideNavigationStory",props:{lists:{defaultValue:null,description:"",name:"lists",required:!0,type:{name:"SideNavigationList[]"}}}}}catch{}const N={title:"Components/Side navigation/Html Implementation",id:"html-side-navigation",component:h,argTypes:B,args:{},parameters:{status:{type:"BETA"},docs:{source:{transform:(r,t)=>{var o,n;const c=typeof t.component=="function"?t.component:typeof((o=t.component)==null?void 0:o.render)=="function"?(n=t.component)==null?void 0:n.render:null;return c?b.format(z(c(t.args)),{parser:"babel",plugins:[S]}):r}}}}},i={args:{lists:[{items:[{text:"Overzicht",href:"#",icon:e.jsx(j,{}),current:!0},{text:"Berichten",href:"#",icon:e.jsx(I,{}),badge:9},{text:"Lopende zaken",href:"#",icon:e.jsx(x,{})},{text:"Belastingzaken",href:"#",icon:e.jsx(y,{})},{text:"WOZ",href:"#",icon:e.jsx(k,{})},{text:"Parkeren",href:"#",icon:e.jsx(_,{})},{text:"Erfpacht",href:"#",icon:e.jsx(f,{})},{text:"Gegevens",href:"#",icon:e.jsx(u,{})}]}]}},s={args:{lists:[{items:[{text:"Overzicht",href:"#",icon:e.jsx(j,{}),current:!0}]},{items:[{text:"Berichten",href:"#",icon:e.jsx(I,{}),badge:9},{text:"Lopende zaken",href:"#",icon:e.jsx(x,{})}]},{items:[{text:"Belastingzaken",href:"#",icon:e.jsx(y,{})},{text:"WOZ",href:"#",icon:e.jsx(k,{})},{text:"Parkeren",href:"#",icon:e.jsx(_,{})},{text:"Erfpacht",href:"#",icon:e.jsx(f,{})}]},{items:[{text:"Gegevens",href:"#",icon:e.jsx(u,{})}]}]}};var d,m,p;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    lists: [{
+      items: [{
+        text: 'Overzicht',
+        href: '#',
+        icon: <IconLayoutGrid />,
+        current: true
+      }, {
+        text: 'Berichten',
+        href: '#',
+        icon: <IconInbox />,
+        badge: 9
+      }, {
+        text: 'Lopende zaken',
+        href: '#',
+        icon: <IconArchive />
+      }, {
+        text: 'Belastingzaken',
+        href: '#',
+        icon: <IconCurrencyEuro />
+      }, {
+        text: 'WOZ',
+        href: '#',
+        icon: <IconHome />
+      }, {
+        text: 'Parkeren',
+        href: '#',
+        icon: <IconParking />
+      }, {
+        text: 'Erfpacht',
+        href: '#',
+        icon: <IconBuildingCommunity />
+      }, {
+        text: 'Gegevens',
+        href: '#',
+        icon: <IconUser />
+      }]
+    }]
+  }
+}`,...(p=(m=i.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var g,l,v;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    lists: [{
+      items: [{
+        text: 'Overzicht',
+        href: '#',
+        icon: <IconLayoutGrid />,
+        current: true
+      }]
+    }, {
+      items: [{
+        text: 'Berichten',
+        href: '#',
+        icon: <IconInbox />,
+        badge: 9
+      }, {
+        text: 'Lopende zaken',
+        href: '#',
+        icon: <IconArchive />
+      }]
+    }, {
+      items: [{
+        text: 'Belastingzaken',
+        href: '#',
+        icon: <IconCurrencyEuro />
+      }, {
+        text: 'WOZ',
+        href: '#',
+        icon: <IconHome />
+      }, {
+        text: 'Parkeren',
+        href: '#',
+        icon: <IconParking />
+      }, {
+        text: 'Erfpacht',
+        href: '#',
+        icon: <IconBuildingCommunity />
+      }]
+    }, {
+      items: [{
+        text: 'Gegevens',
+        href: '#',
+        icon: <IconUser />
+      }]
+    }]
+  }
+}`,...(v=(l=s.parameters)==null?void 0:l.docs)==null?void 0:v.source}}};const E=["Default","Grouped"],A=Object.freeze(Object.defineProperty({__proto__:null,Default:i,Grouped:s,__namedExportsOrder:E,default:N},Symbol.toStringTag,{value:"Module"}));export{i as D,s as G,A as S};

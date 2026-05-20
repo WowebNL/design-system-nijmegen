ed480e79e8c76e41c6a82cb1a43221ff19d6e494
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2026-05-20 08:13:26 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@c0382011aacb7f84cd341d4e936ef16ea74b8805 🚀

---
+++ assets/NumberBadge.docs-PVM0f7Bu.js
@@ -0,0 +1,25 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as m,P as i,b as r,e as p}from"./chunk-HLWAVYOI-lQm16UoX.js";import{N as a}from"./NumberBadge.stories-oo1b5D-I.js";import{useMDXComponents as o}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BvfMsYST.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";/* empty css                  */const l=`<!doctype html>
+<html lang="nl">
+  <head>
+    <title>Note example</title>
+    <!-- global -->
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/document-css@1.5.0/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/design-tokens@latest/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/font@latest/dist/index.css" />
+    <!-- component specific -->
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/components-css@latest/dist/index.min.css" />
+  </head>
+
+  <body class="utrecht-document nijmegen-theme">
+    <span class="nijmegen-number-badge" role="status">9</span>
+  </body>
+</html>
+`;function s(n){const t=Object.assign({h1:"h1",h2:"h2"},o(),n.components);return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(m,{of:a}),`
+`,e.jsx(t.h1,{id:"number-badge-component-implementation-in-htmlcss",children:"Number badge Component implementation in HTML/CSS"}),`
+`,e.jsx(t.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,e.jsx(i,{}),`
+`,e.jsx(r,{}),`
+`,e.jsx(t.h2,{id:"example-implementation",children:"Example implementation"}),`
+`,e.jsx(p,{language:"html",code:l})]})}function O(n={}){const{wrapper:t}=Object.assign({},o(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(s,n)})):s(n)}export{O as default};

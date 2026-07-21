fe2ad0511a9d6895963291ff1d1a7e1e0cfec834
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2026-07-21 09:48:35 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@d518beea856a1049f9168f9427399285164667fe 🚀

---
+++ assets/Blockquote.docs-DMNo5swM.js
@@ -0,0 +1,30 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as i,P as m,b as c,e as l}from"./chunk-HLWAVYOI-CVr0rVh0.js";import{B as r}from"./Blockquote.stories-Dub2uXdo.js";import{useMDXComponents as s}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Dmx8DIBG.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";/* empty css                  */const p=`<!doctype html>
+<html lang="nl">
+  <head>
+    <title>Blockquote example</title>
+    <!-- global -->
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/document-css@1.5.0/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/design-tokens@latest/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/font@latest/dist/index.css" />
+    <!-- component specific -->
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/components-css@latest/dist/index.min.css" />
+  </head>
+
+  <body class="utrecht-document nijmegen-theme">
+    <figure class="nijmegen-blockquote">
+      <blockquote class="nijmegen-blockquote__quote">
+        <p class="nijmegen-paragraph">Ondernemers en bedrijven uit Nijmegen opgelet!</p>
+      </blockquote>
+      <figcaption class="nijmegen-blockquote__caption">— L. Ipsum</figcaption>
+    </figure>
+  </body>
+</html>
+`;function o(n){const t=Object.assign({h1:"h1",h2:"h2"},s(),n.components);return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{of:r}),`
+`,e.jsx(t.h1,{id:"blockquote-component-implementation-in-htmlcss",children:"Blockquote Component implementation in HTML/CSS"}),`
+`,e.jsx(t.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,e.jsx(m,{}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"example-implementation",children:"Example implementation"}),`
+`,e.jsx(l,{language:"html",code:p})]})}function O(n={}){const{wrapper:t}=Object.assign({},s(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(o,n)})):o(n)}export{O as default};
--- assets/Separator.docs-uDW6pw6U.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as m,P as i,b as s}from"./chunk-HLWAVYOI-CpHipq6h.js";import{S as p}from"./Separator.stories-Dxz5GRC3.js";import{useMDXComponents as e}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BsgNsN2S.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./index-DP2ZV-sn.js";import"./index-7bwtWdbn.js";import"./server.browser-rAMDWieH.js";function r(o){const n=Object.assign({h1:"h1",h2:"h2"},e(),o.components);return t.jsxs(t.Fragment,{children:[`
-`,`
-`,t.jsx(m,{of:p}),`
-`,t.jsx(n.h1,{id:"separator-component-implementation-in-htmlcss",children:"Separator Component implementation in HTML/CSS"}),`
-`,t.jsx(n.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
-`,t.jsx(i,{}),`
-`,t.jsx(s,{})]})}function y(o={}){const{wrapper:n}=Object.assign({},e(),o.components);return n?t.jsx(n,Object.assign({},o,{children:t.jsx(r,o)})):r(o)}export{y as default};

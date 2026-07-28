fe1eaa65ac8608aa8ae40fd91ba8b0325318a0d9
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2026-07-28 11:11:34 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@471c2466c4bfee20f7694f6913755150028b57e3 🚀

---
+++ assets/Hero.stories-B95JpPCU.js
@@ -0,0 +1,51 @@
+import{s as L,r as q,p as w}from"./server.browser-rAMDWieH.js";import{j as e}from"./jsx-runtime-CKrituN3.js";import{d as B,P as T}from"./index-DP2ZV-sn.js";import{c as A}from"./clsx-B-dksMZM.js";import{b as E,S as O}from"./_Search-Bla5NiKE.js";/* empty css                  */const P={title:{name:"Title",control:"text",table:{defaultValue:""}},text:{name:"Text",control:"text",table:{defaultValue:""}},image:{name:"Image",control:"text",table:{defaultValue:""}},image_alt:{name:"Image alt",control:"text",table:{defaultValue:""}},button_group:{name:"Show button group",control:{type:"boolean"}},type:{name:"Type",control:{type:"select"},options:["home","category","image","background-image"]},autocomplete:{name:"Autocomplete example",control:"boolean"}},l=({title:o="",text:t="",image:r="",image_alt:u="",type:a="",button_group:N=!1,autocomplete:V=!1})=>e.jsxs("section",{className:A("nijmegen-hero",{"nijmegen-hero--home":a==="home","nijmegen-hero--background-image":a==="background-image","nijmegen-hero--image":a==="image"}),role:"region",children:[e.jsx("div",{className:"nijmegen-hero__container",children:e.jsxs("div",{className:"nijmegen-hero__content",children:[e.jsxs("div",{className:"nijmegen-hero__content--heading",children:[o&&e.jsx(B,{children:o}),t&&a==="home"&&e.jsx("p",{className:"utrecht-heading-2",children:t})]}),t&&a!=="home"&&e.jsx(T,{children:t}),a==="home"&&(V?e.jsx(E,{dark:!0,autocomplete:!0}):e.jsx(O,{dark:!0})),N===!0&&e.jsxs("p",{role:"group",className:"utrecht-button-group",children:[e.jsx("button",{className:"utrecht-button utrecht-button--primary-action",type:"button",children:"Button"}),e.jsx("button",{className:"utrecht-button utrecht-button--secondary-action",type:"button",children:"Button"})]})]})}),r&&a!=="category"&&e.jsx("img",{className:"nijmegen-hero__image",src:r,alt:u})]});try{l.displayName="HeroStory",l.__docgenInfo={description:"",displayName:"HeroStory",props:{title:{defaultValue:{value:""},description:"",name:"title",required:!1,type:{name:"string"}},text:{defaultValue:{value:""},description:"",name:"text",required:!1,type:{name:"string"}},image:{defaultValue:{value:""},description:"",name:"image",required:!1,type:{name:"string"}},image_alt:{defaultValue:{value:""},description:"",name:"image_alt",required:!1,type:{name:"string"}},type:{defaultValue:{value:""},description:"",name:"type",required:!1,type:{name:"string"}},button_group:{defaultValue:{value:"false"},description:"",name:"button_group",required:!1,type:{name:"boolean"}},autocomplete:{defaultValue:{value:"false"},description:"",name:"autocomplete",required:!1,type:{name:"boolean"}}}}}catch{}const D={title:"Components/Hero/Html Implementation",id:"html-hero",argTypes:P,component:l,args:{},parameters:{status:{type:"BETA"},docs:{source:{transform:(o,t)=>{var u,a;const r=typeof t.component=="function"?t.component:typeof((u=t.component)==null?void 0:u.render)=="function"?(a=t.component)==null?void 0:a.render:null;return r?L.format(q(r(t.args)),{parser:"babel",plugins:[w]}):o}}}}},n={name:"Hero homepage",args:{title:"Heading",text:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod",image:"https://picsum.photos/640/360/",image_alt:"Image description",button_group:!1,autocomplete:!1,type:"home"}},s={name:"Hero homepage, autocomplete example",args:{title:"Heading",text:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod",image:"https://picsum.photos/640/360/",image_alt:"Image description",button_group:!1,autocomplete:!0,type:"home"}},i={name:"Hero category, with image",args:{title:"Heading",text:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod",image:"https://picsum.photos/640/360/",image_alt:"Image description",button_group:!1,type:"image"}},m={name:"Hero category, without Image",args:{title:"Heading",text:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod",button_group:!1,type:"category"}},c={name:"Hero category, with background image",args:{title:"Heading",text:"Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod",image:"https://picsum.photos/640/360/",image_alt:"Image description",button_group:!0,type:"background-image"}};var p,d,g;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  name: 'Hero homepage',
+  args: {
+    title: 'Heading',
+    text: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    image: 'https://picsum.photos/640/360/',
+    image_alt: 'Image description',
+    button_group: false,
+    autocomplete: false,
+    type: 'home'
+  }
+}`,...(g=(d=n.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var h,f,b;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  name: 'Hero homepage, autocomplete example',
+  args: {
+    title: 'Heading',
+    text: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    image: 'https://picsum.photos/640/360/',
+    image_alt: 'Image description',
+    button_group: false,
+    autocomplete: true,
+    type: 'home'
+  }
+}`,...(b=(f=s.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var _,y,x;i.parameters={...i.parameters,docs:{...(_=i.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  name: 'Hero category, with image',
+  args: {
+    title: 'Heading',
+    text: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    image: 'https://picsum.photos/640/360/',
+    image_alt: 'Image description',
+    button_group: false,
+    type: 'image'
+  }
+}`,...(x=(y=i.parameters)==null?void 0:y.docs)==null?void 0:x.source}}};var H,j,I;m.parameters={...m.parameters,docs:{...(H=m.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  name: 'Hero category, without Image',
+  args: {
+    title: 'Heading',
+    text: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    button_group: false,
+    type: 'category'
+  }
+}`,...(I=(j=m.parameters)==null?void 0:j.docs)==null?void 0:I.source}}};var S,k,v;c.parameters={...c.parameters,docs:{...(S=c.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  name: 'Hero category, with background image',
+  args: {
+    title: 'Heading',
+    text: 'Lorem ipsum dolor sit amet, consectetur ad isicing elit, sed do eiusmod',
+    image: 'https://picsum.photos/640/360/',
+    image_alt: 'Image description',
+    button_group: true,
+    type: 'background-image'
+  }
+}`,...(v=(k=c.parameters)==null?void 0:k.docs)==null?void 0:v.source}}};const M=["Default","Autocomplete","Image","Category","BackgroundImage"],J=Object.freeze(Object.defineProperty({__proto__:null,Autocomplete:s,BackgroundImage:c,Category:m,Default:n,Image:i,__namedExportsOrder:M,default:D},Symbol.toStringTag,{value:"Module"}));export{s as A,c as B,m as C,J as H,i as I};
---
+++ assets/TaskCard.docs-D-xT9E9Q.js
@@ -0,0 +1,39 @@
+import{j as n}from"./jsx-runtime-CKrituN3.js";import{M as m,P as o,b as a,e as r}from"./chunk-HLWAVYOI-BB-Y_ghr.js";import{T as c}from"./TaskCard.stories-DpXVTJ_w.js";import{useMDXComponents as i}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DPB_e16T.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";/* empty css                  *//* empty css              */import"./IconAlertTriangle-C8yEghIP.js";import"./createReactComponent-D_5DJZVZ.js";const d=`<!doctype html>
+<html lang="nl">
+  <head>
+    <title>Task card example</title>
+    <!-- global -->
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/document-css@1.5.0/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/design-tokens@latest/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/font@latest/dist/index.css" />
+    <!-- component specific -->
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/components-css@latest/dist/index.min.css" />
+  </head>
+
+  <body class="utrecht-document nijmegen-theme">
+    <a class="nijmegen-task-card" href="#example">
+      <div class="nijmegen-task-card__content">
+        <strong>Task</strong>
+      </div>
+      <div class="nijmegen-task-card__context">
+        <div class="nijmegen-task-card__details">
+          <div class="nijmegen-task-card__date">
+            <time datetime=""></time>
+          </div>
+        </div>
+        <div class="nijmegen-task-card__actions">
+          <span class="utrecht-button utrecht-button--primary-action">Text</span>
+        </div>
+      </div>
+    </a>
+  </body>
+</html>
+`;function s(t){const e=Object.assign({h1:"h1",h2:"h2"},i(),t.components);return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(m,{of:c}),`
+`,n.jsx(e.h1,{id:"task-card-component-implementation-in-htmlcss",children:"Task card Component implementation in HTML/CSS"}),`
+`,n.jsx(e.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,n.jsx(o,{}),`
+`,n.jsx(a,{}),`
+`,n.jsx(e.h2,{id:"example-implementation",children:"Example implementation"}),`
+`,n.jsx(r,{language:"html",code:d})]})}function X(t={}){const{wrapper:e}=Object.assign({},i(),t.components);return e?n.jsx(e,Object.assign({},t,{children:n.jsx(s,t)})):s(t)}export{X as default};

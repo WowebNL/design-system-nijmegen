959f06768db57b61210fa4cb9ae0d650362bb599
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2026-09-04 11:34:11 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@097bad3361b48aae09da5e3cd27e8269a79107be 🚀

---
+++ assets/Alert.docs-C2q8ZWJ3.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as i,P as e,b as s}from"./chunk-HLWAVYOI-CBZ3gD0r.js";import{A as p}from"./Alert.stories-CDPlFQUr.js";import{useMDXComponents as m}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CJeiUXWk.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";import"./_Alert-CvFcGbra.js";import"./index-DP2ZV-sn.js";import"./index-7bwtWdbn.js";import"./IconInfoCircle-Bw9o0Bye.js";import"./createReactComponent-D_5DJZVZ.js";import"./IconAlertTriangle-C8yEghIP.js";function r(o){const n=Object.assign({h1:"h1",h2:"h2"},m(),o.components);return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(i,{of:p}),`
+`,t.jsx(n.h1,{id:"alert-component-implementation-in-htmlcss",children:"Alert Component implementation in HTML/CSS"}),`
+`,t.jsx(n.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,t.jsx(e,{}),`
+`,t.jsx(s,{})]})}function _(o={}){const{wrapper:n}=Object.assign({},m(),o.components);return n?t.jsx(n,Object.assign({},o,{children:t.jsx(r,o)})):r(o)}export{_ as default};

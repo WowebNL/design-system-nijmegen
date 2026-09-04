8963d35319afbc4ca36c177dd9862df50449cbb4
mwbankers87 <118347508+mwbankers87@users.noreply.github.com>
2026-09-04 08:34:49 +0000

    Deploying to gh-pages from @ WowebNL/design-system-nijmegen@b12d902f23ffcf09c1f31f71a3910a6b3625de98 🚀

---
+++ assets/Documentatie-Ja29DCW6.js
@@ -0,0 +1,12 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{C as i}from"./chunk-HLWAVYOI-BS9tmBaH.js";import{D as a,a as s,b as d,c as m}from"./TaskCard.stories-Di6-g3ih.js";import{useMDXComponents as r}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BuQxZ52l.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./server.browser-rAMDWieH.js";/* empty css                  *//* empty css              */import"./IconAlertTriangle-C8yEghIP.js";import"./createReactComponent-D_5DJZVZ.js";function o(n){const e=Object.assign({h1:"h1",h2:"h2",p:"p",table:"table",thead:"thead",tr:"tr",th:"th",tbody:"tbody",td:"td",a:"a",h3:"h3"},r(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(e.h1,{id:"documentatie-task-card-component",children:"Documentatie Task card component"}),`
+`,t.jsx(e.h2,{id:"component",children:"Component"}),`
+`,t.jsx(e.p,{children:"Dit component is op maat ontwikkeld op basis van het ontwerp voor de Gemeente Nijmegen."}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Link"}),t.jsx(e.th,{children:"Omschrijving"})]})}),t.jsx(e.tbody,{children:t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.a,{href:"https://www.figma.com/design/g3AieWAFD3NtkbDDTTFC30/Novio---Nijmegen-Design-System?node-id=9921-75938",target:"_blank",rel:"nofollow noopener noreferrer",children:"Figma gemeente Nijmegen"})}),t.jsx(e.td,{children:"Design documentatie van dit component."})]})})]}),`
+`,t.jsx(e.h2,{id:"variaties",children:"Variaties"}),`
+`,t.jsx(i,{of:a,sourceState:"none"}),`
+`,t.jsx(e.h3,{id:"with-date",children:"With date"}),`
+`,t.jsx(i,{of:s,sourceState:"none"}),`
+`,t.jsx(e.h3,{id:"with-date-relative",children:"With Date Relative"}),`
+`,t.jsx(i,{of:d,sourceState:"none"}),`
+`,t.jsx(e.h3,{id:"with-date-warning",children:"With Date Warning"}),`
+`,t.jsx(i,{of:m,sourceState:"none"})]})}function F(n={}){const{wrapper:e}=Object.assign({},r(),n.components);return e?t.jsx(e,Object.assign({},n,{children:t.jsx(o,n)})):o(n)}export{F as default};
---
+++ assets/StepList.docs-XDknY_uS.js
@@ -0,0 +1,83 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{M as a,P as o,b as l,e as c}from"./chunk-HLWAVYOI-BS9tmBaH.js";import{S as r}from"./StepList.stories-Dl0vsikW.js";import{useMDXComponents as i}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BuQxZ52l.js";import"../sb-preview/runtime.js";import"./react-18-D8cruF67.js";import"./index-BtM5VmRH.js";import"./index-CnWNB3mc.js";import"./extends-CCbyfPlC.js";import"./_getPrototype-nBa_wnor.js";import"./index-ltOY3w3e.js";import"./index-DP2ZV-sn.js";import"./index-7bwtWdbn.js";import"./server.browser-rAMDWieH.js";/* empty css                  */const m=`<!doctype html>
+<html lang="nl">
+  <head>
+    <title>Steplist example</title>
+    <!-- global -->
+    <link rel="stylesheet" href="https://unpkg.com/@utrecht/document-css@1.5.0/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/design-tokens@latest/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/font@latest/dist/index.css" />
+    <!-- component specific -->
+    <link rel="stylesheet" href="https://unpkg.com/@gemeentenijmegen/components-css@latest/dist/index.min.css" />
+  </head>
+
+  <body class="utrecht-document nijmegen-theme">
+    <ol class="nijmegen-step-list">
+      <li class="nijmegen-step-list__item">
+        <div class="nijmegen-step-list__header">
+          <h5 class="utrecht-heading-5">Heading</h5>
+        </div>
+        <div class="nijmegen-step-list__body">
+          <p class="utrecht-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+        </div>
+      </li>
+      <li class="nijmegen-step-list__item">
+        <div class="nijmegen-step-list__header">
+          <h5 class="utrecht-heading-5">Heading</h5>
+        </div>
+        <div class="nijmegen-step-list__body">
+          <p class="utrecht-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+          <button class="utrecht-button utrecht-button--primary-action" type="button">Text</button>
+          <button class="utrecht-button utrecht-button--secondary-action" type="button">Text</button>
+        </div>
+      </li>
+      <li class="nijmegen-step-list__item">
+        <div class="nijmegen-step-list__header">
+          <h5 class="utrecht-heading-5">Heading</h5>
+        </div>
+        <div class="nijmegen-step-list__body">
+          <p class="utrecht-paragraph">Different content for the third step. This can be any text you want.</p>
+          <a href="#" class="utrecht-link utrecht-link--html-a"> Text </a>
+        </div>
+      </li>
+    </ol>
+
+    <ul class="nijmegen-step-list">
+      <li class="nijmegen-step-list__item">
+        <div class="nijmegen-step-list__header">
+          <h5 class="utrecht-heading-5">Heading</h5>
+        </div>
+        <div class="nijmegen-step-list__body">
+          <p class="utrecht-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+        </div>
+      </li>
+      <li class="nijmegen-step-list__item">
+        <div class="nijmegen-step-list__header">
+          <h5 class="utrecht-heading-5">Heading</h5>
+        </div>
+        <div class="nijmegen-step-list__body">
+          <p class="utrecht-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+          <button class="utrecht-button utrecht-button--primary-action" type="button">Text</button>
+          <button class="utrecht-button utrecht-button--secondary-action" type="button">Text</button>
+        </div>
+      </li>
+      <li class="nijmegen-step-list__item">
+        <div class="nijmegen-step-list__header">
+          <h5 class="utrecht-heading-5">Heading</h5>
+        </div>
+        <div class="nijmegen-step-list__body">
+          <p class="utrecht-paragraph">Different content for the third step. This can be any text you want.</p>
+          <a href="#" class="utrecht-link utrecht-link--html-a"> Text </a>
+        </div>
+      </li>
+    </ul>
+  </body>
+</html>
+`;function s(e){const n=Object.assign({h1:"h1",h2:"h2"},i(),e.components);return t.jsxs(t.Fragment,{children:[`
+`,`
+`,t.jsx(a,{of:r}),`
+`,t.jsx(n.h1,{id:"step-list-component-implementation-in-htmlcss",children:"Step list Component implementation in HTML/CSS"}),`
+`,t.jsx(n.h2,{id:"component-suported-arguments",children:"Component suported arguments"}),`
+`,t.jsx(o,{}),`
+`,t.jsx(l,{}),`
+`,t.jsx(n.h2,{id:"example-implementation",children:"Example implementation"}),`
+`,t.jsx(c,{language:"html",code:m})]})}function S(e={}){const{wrapper:n}=Object.assign({},i(),e.components);return n?t.jsx(n,Object.assign({},e,{children:t.jsx(s,e)})):s(e)}export{S as default};
